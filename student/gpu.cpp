597345dd8cd378f699415901f91a804143e0e2ce
marekuzel <marekuzel1@gmail.com>
2023-05-03 00:39:08 +0200

    First test passed

--- student/gpu.cpp
+++ student/gpu.cpp
@@ -7,12 +7,50 @@
 
 #include <student/gpu.hpp>
 
-
+void clear(GPUMemory&mem,ClearCommand cmd){
+  
+  auto & frame = mem.framebuffer;
+    auto nOfPixels = frame.height*frame.width;
+    for (int i = 0; i < nOfPixels; ++i){
+      if(cmd.clearColor && !(cmd.clearDepth)){  
+        float red = cmd.color.r;
+        mem.framebuffer.color[i*4] = (uint8_t)red*255.f;
+        float green = cmd.color.g;
+        mem.framebuffer.color[i*4+1] = (uint8_t)green*255.f;
+        float blue = cmd.color.b;
+        mem.framebuffer.color[i*4+2] = (uint8_t)blue*255.f;
+        float aplha = cmd.color.a;
+        mem.framebuffer.color[i*4+3] = (uint8_t)aplha*255.f;
+        
+      }
+      else if (cmd.clearColor && cmd.clearDepth){
+        float red = cmd.color.r;
+        mem.framebuffer.color[i*4] = (uint8_t)red*255.f;
+        float green = cmd.color.g;
+        mem.framebuffer.color[i*4+1] = (uint8_t)green*255.f;
+        float blue = cmd.color.b;
+        mem.framebuffer.color[i*4+2] = (uint8_t)blue*255.f;
+        float aplha = cmd.color.a;
+        mem.framebuffer.color[i*4+3] = (uint8_t)aplha*255.f;
+        mem.framebuffer.depth[i] = cmd.depth;
+      }
+      else{
+        mem.framebuffer.depth[i] = cmd.depth;
+      }
+  }
+}
 
 //! [gpu_execute]
 void gpu_execute(GPUMemory&mem,CommandBuffer &cb){
   (void)mem;
   (void)cb;
+  for(uint32_t i=0;i<cb.nofCommands;++i){
+    CommandType type = cb.commands[i].type;
+    CommandData data = cb.commands[i].data;
+    if(type == CommandType::CLEAR){
+      clear(mem,data.clearCommand);
+    }
+  }
   /// \todo Tato funkce reprezentuje funkcionalitu grafické karty.<br>
   /// Měla by umět zpracovat command buffer, čistit framebuffer a kresli.<br>
   /// mem obsahuje paměť grafické karty.
