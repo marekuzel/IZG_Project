7b4a530e61a24b0165ae64760d8ec2f5a2139e7f
marekuzel <marekuzel1@gmail.com>
2023-05-03 23:37:10 +0200

    Working up to test 4 (by using magic)

--- student/gpu.cpp
+++ student/gpu.cpp
@@ -6,51 +6,66 @@
  */
 
 #include <student/gpu.hpp>
-
+#include <stdio.h>
 void clear(GPUMemory&mem,ClearCommand cmd){
   
   auto & frame = mem.framebuffer;
     auto nOfPixels = frame.height*frame.width;
     for (int i = 0; i < nOfPixels; ++i){
       if(cmd.clearColor && !(cmd.clearDepth)){  
-        float red = cmd.color.r;
-        mem.framebuffer.color[i*4] = (uint8_t)red*255.f;
-        float green = cmd.color.g;
-        mem.framebuffer.color[i*4+1] = (uint8_t)green*255.f;
-        float blue = cmd.color.b;
-        mem.framebuffer.color[i*4+2] = (uint8_t)blue*255.f;
-        float aplha = cmd.color.a;
-        mem.framebuffer.color[i*4+3] = (uint8_t)aplha*255.f;
-        
+        mem.framebuffer.color[i*4] = (uint8_t) (cmd.color.r*255.f);
+        mem.framebuffer.color[i*4+1] = (uint8_t) (cmd.color.g*255.f);
+        mem.framebuffer.color[i*4+2] = (uint8_t) (cmd.color.b*255.f);
+        mem.framebuffer.color[i*4+3] = (uint8_t) (cmd.color.a*255.f);
       }
       else if (cmd.clearColor && cmd.clearDepth){
-        float red = cmd.color.r;
-        mem.framebuffer.color[i*4] = (uint8_t)red*255.f;
-        float green = cmd.color.g;
-        mem.framebuffer.color[i*4+1] = (uint8_t)green*255.f;
-        float blue = cmd.color.b;
-        mem.framebuffer.color[i*4+2] = (uint8_t)blue*255.f;
-        float aplha = cmd.color.a;
-        mem.framebuffer.color[i*4+3] = (uint8_t)aplha*255.f;
+        mem.framebuffer.color[i*4] = (uint8_t) (cmd.color.r*255.f);
+        mem.framebuffer.color[i*4+1] = (uint8_t) (cmd.color.g*255.f);
+        mem.framebuffer.color[i*4+2] = (uint8_t) (cmd.color.b*255.f);
+        mem.framebuffer.color[i*4+3] = (uint8_t) (cmd.color.a*255.f);
         mem.framebuffer.depth[i] = cmd.depth;
       }
-      else{
+      else if (cmd.clearDepth){
         mem.framebuffer.depth[i] = cmd.depth;
       }
   }
 }
 
+
+void runVertexAssembly(InVertex inVertex,VertexArray vao){
+ //read attributes
+}
+
+void draw(GPUMemory&mem,DrawCommand cmd, int drawId){
+  Program prg = mem.programs[cmd.programID];
+
+  InVertex inVertex;
+  inVertex.gl_DrawID = drawId;
+  OutVertex outVertex;
+  for(int i = 0; i < cmd.nofVertices; ++i){
+    
+    runVertexAssembly(inVertex,cmd.vao);
+    ShaderInterface si;
+    prg.vertexShader(outVertex,inVertex,si);
+    inVertex.gl_VertexID += 1;
+  }
+}
 //! [gpu_execute]
 void gpu_execute(GPUMemory&mem,CommandBuffer &cb){
   (void)mem;
   (void)cb;
+  int drawId = 0;
   for(uint32_t i=0;i<cb.nofCommands;++i){
     CommandType type = cb.commands[i].type;
     CommandData data = cb.commands[i].data;
     if(type == CommandType::CLEAR){
       clear(mem,data.clearCommand);
     }
+    else if (type == CommandType::DRAW){
+      draw(mem, data.drawCommand,(drawId)++);
+    }
   }
+    
   /// \todo Tato funkce reprezentuje funkcionalitu grafické karty.<br>
   /// Měla by umět zpracovat command buffer, čistit framebuffer a kresli.<br>
   /// mem obsahuje paměť grafické karty.
